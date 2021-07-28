23de888e9acc084754c49fe524d27cc022fddab0
dav-jones <65011654+dav-jones@users.noreply.github.com>
2021-07-28 17:34:06 +0100

    push error fix

--- main.py
+++ main.py
@@ -1,84 +1,81 @@
-from flask import Flask, request
-
+### import python modules
 import time
 import datetime
 import http.client
 import json
 import pandas as pd
+import streamlit as st
 
-app = Flask(__name__)
+### set page config and title
+st.set_page_config(page_title='Cod Tournament')
+st.title('Cod Tournament')
+st.info('Make your selection and then click start to begin a tournament')
+st.error('There has been an error, refresh the page to start again')
+st.success('Done!')
 
-### example time flask function
-@app.route('/time')
-def get_current_time():
-    return {'time': datetime.datetime.now().astimezone().replace(microsecond=0)}
+### create selection sidebar
+platform = st.sidebar.selectbox('Platform:', ['psn','xbl'])
+gamemode = st.sidebar.selectbox('Game mode:', ['Resurgence Duos','Resurgence Trios','Resurgence Quads'])
+date = st.sidebar.date_input('Tournament date')
+start_time = st.sidebar.time_input('Start time')
+end_time= st.sidebar.time_input('End time')
+players = st.sidebar.text_input('Usernames (comma separated)')
+st.sidebar.button('Start')
 
-@app.route('/api/query', methods = ['POST'])
-def get_query_from_react():
-    ### get data from request body, set variables
-    data = request.get_json()
+### input variables to build leaderboard
+platform = 'psn'
+players = ['Dav-Jones','ws23100']
+gamemode = 'Resurgence Trios'
+duration_hours = 2
+start_time = datetime.datetime.strptime('2021-07-22 20:00:00+00:00', '%Y-%m-%d %H:%M:%S%z')
 
-    players = ['Dav-Jones','MonsieurOmlette','connarm23']
-    gamemode = 'Resurgence Quads'
-    duration_hours = 2
-    start_time = datetime.datetime.strptime('2021-03-20 00:00:00+00:00', '%Y-%m-%d %H:%M:%S%z')
+### calculate tournament end time
+end_time = start_time + datetime.timedelta(hours = duration_hours)
 
-    platform = data['platform']
-    # players = data['players']
-    # gamemode = data['gamemode']
-    # duration_hours = data['duration_hours']
-    # start_time = data['start_time']
+### dataframe that collects match data for all usernames in players
+all_matches = pd.DataFrame()
+for username in players:
 
-    ### calculate tournament end time
-    end_time = start_time + datetime.timedelta(hours = duration_hours)
+    ### get tracker.gg json object using api
+    conn = http.client.HTTPSConnection('api.tracker.gg')
+    payload = ''
+    conn.request('GET', '/api/v2/warzone/standard/matches/' + platform + '/' + username + '?type=wz', payload)
+    res = conn.getresponse()
+    history_dict = json.loads(res.read())
 
-    ### dataframe that collects match data for all usernames in players
-    all_matches = pd.DataFrame()
-    for username in players:
+    ### parse only the match details
+    match_list = history_dict['data']['matches']
 
-        ### get tracker.gg json object using api
-        conn = http.client.HTTPSConnection('api.tracker.gg')
-        payload = ''
-        conn.request('GET', '/api/v2/warzone/standard/matches/' + platform + '/' + username + '?type=wz', payload)
-        res = conn.getresponse()
-        history_dict = json.loads(res.read())
+    ### create empty list and append details for dataframe conversion
+    d = []
+    for match in match_list:
+        d.append((
+            username,
+            match['attributes']['id'],
+            ### convert time to ISO 8601 timestamp
+            datetime.datetime.strptime((match['metadata']['timestamp']), '%Y-%m-%dT%H:%M:%S%z'),
+            match['metadata']['modeName'],
+            match['segments'][0]['metadata']['placement'],
+            match['segments'][0]['stats']['kills']['value']
+        ))
 
-        ### parse only the match details
-        match_list = history_dict['data']['matches']
+    player_matches = pd.DataFrame(d, columns=('username', 'id', 'timestamp', 'gamemode', 'placement', 'kills'))
+    all_matches = all_matches.append(player_matches)
+    
+### match details between tournament start and end times (copy() to get rid of SettingWithCopyWarning)
+tournament_matches = all_matches[(all_matches['gamemode'] == gamemode) & (all_matches['timestamp'] >= start_time) & (all_matches['timestamp'] < end_time)].copy()
 
-        ### create empty list and append details for dataframe conversion
-        d = []
-        for match in match_list:
-            d.append((
-                username,
-                match['attributes']['id'],
-                ### convert time to ISO 8601 timestamp
-                datetime.datetime.strptime((match['metadata']['timestamp']), '%Y-%m-%dT%H:%M:%S%z'),
-                match['metadata']['modeName'],
-                match['segments'][0]['metadata']['placement'],
-                match['segments'][0]['stats']['kills']['value']
-            ))
+### calculate points based on placement and kills for each match
+tournament_matches.loc[tournament_matches.placement <= 5, 'placement_points'] = 6 - tournament_matches.placement
+tournament_matches.loc[tournament_matches.placement > 5, 'placement_points'] = 0                                        
+tournament_matches['kill_points'] = tournament_matches['kills']
 
-        player_matches = pd.DataFrame(d, columns=('username', 'id', 'timestamp', 'gamemode', 'placement', 'kills'))
-        all_matches = all_matches.append(player_matches)
+### aggregate points and rank usernames - ranking not working
+leaderboard = tournament_matches.groupby('username').sum()
+leaderboard.drop(['placement','kills'], axis=1, inplace=True)
+leaderboard['total_points'] = leaderboard['placement_points'] + leaderboard['kill_points']
+leaderboard.sort_values('total_points', ascending = True)
+leaderboard.reset_index(inplace=True)
+leaderboard.insert(1, 'rank', leaderboard.index.values + 1)
 
-    ### match details between tournament start and end times (copy() to get rid of SettingWithCopyWarning)
-    tournament_matches = all_matches[(all_matches['gamemode'] == gamemode) & (all_matches['timestamp'] >= start_time) & (all_matches['timestamp'] < end_time)].copy()
-
-    ### calculate points based on placement and kills for each match
-    tournament_matches.loc[tournament_matches.placement <= 5, 'placement_points'] = 6 - tournament_matches.placement
-    tournament_matches.loc[tournament_matches.placement > 5, 'placement_points'] = 0                                        
-    tournament_matches['kill_points'] = tournament_matches['kills']
-
-    ### aggregate points and rank usernames
-    leaderboard = tournament_matches.groupby('username').sum()
-    leaderboard.drop(['placement','kills'], axis=1, inplace=True)
-    leaderboard['total_points'] = leaderboard['placement_points'] + leaderboard['kill_points']
-    leaderboard.sort_values(by=['total_points','kill_points'])
-    leaderboard.reset_index(inplace=True)
-    leaderboard.insert(1, 'rank', leaderboard.index.values + 1)
-
-    ### convert to json
-    json_leaderboard = json.dumps(json.loads(leaderboard.to_json(orient='records')), indent=4)
-
-    return json_leaderboard
+st.table(leaderboard)
